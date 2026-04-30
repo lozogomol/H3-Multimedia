649d6da60b6e21596daa7287e0b0de431dbd1abb
itssug <parklove651@gmail.com>
2026-04-29 21:38:26 -0400

    union niveles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import Nivel1 from './Nivel-1/Nivel-1'; 
+import React, { useState } from 'react';
+import Nivel1 from './Nivel-1/Nivel-1';
+import Nivel2 from './Nivel-2/Nivel-2';
 import './App.css';
 
 function App() {
+  const [nivel, setNivel] = useState(1);
+
   return (
     <div className="App">
 
-      <Nivel1 />
+      {nivel === 1 && (
+        <>
+          <Nivel1 />
+          <button onClick={() => setNivel(2)}>
+            Ir al Nivel 2
+          </button>
+        </>
+      )}
+
+      {nivel === 2 && <Nivel2 />}
+
     </div>
   );
 }
